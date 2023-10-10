eb1f083127a8e5c9d1c3e55e0d7c1b0922a809bb
gustavocarlim <gustavocarlimsaens@gmail.com>
2023-10-10 19:31:20 -0300

    req 2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const talkerRouter = require('./routes/talkerRouter');
+const readAndwriteFiles = require('./untils/readAndWriteFiles');
 
 const app = express();
 app.use(express.json());
@@ -11,6 +12,16 @@ app.use('/talker', talkerRouter);
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readAndwriteFiles.readTalker();
+  const selectedTalker = talkers.find((talker) => talker.id === Number(id));
+  if (!selectedTalker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(selectedTalker);
 });
 
 app.listen(PORT, () => {
--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -8,4 +8,14 @@ router.get('/', async (_req, res) => {
   res.status(200).json(talkers);
 });
 
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+  const talkers = await readAndwriteFiles.readTalker();
+  const selectedTalker = talkers.find((talker) => talker.id === Number(id));
+  if (!selectedTalker) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(selectedTalker);
+});
+
 module.exports = router;    
